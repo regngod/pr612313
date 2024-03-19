2fa3ba7fb50e336644ccfbfde05d46c36ac4f8a7
alexandrkorol <oaangrycreeper10@gmail.com>
2024-03-19 18:04:13 +0300

    туц

--- tests/integration.py
+++ tests/integration.py
@@ -1,20 +1,16 @@
-import unittest
-import os
 import sys
-import requests
-import time
-
-# Добавляем путь к модулю delivery_service для корректного импорта
+import os
 delivery_service_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../delivery_service'))
 sys.path.append(delivery_service_path)
 
-# Импортируем необходимые компоненты для тестирования
+import unittest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from delivery_service import create_food_delivery_and_record, FoodDelivery
+from sqlalchemy.exc import IntegrityError
 
-class TestComponent(unittest.TestCase):
 
+class TestDeliveryService(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Создаем тестовую базу данных SQLite и сессию SQLAlchemy
@@ -27,46 +23,35 @@ class TestComponent(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # Очищаем таблицы после выполнения всех тестов
+        # Очищаем таблицы после выполнения тестов
         FoodDelivery.__table__.drop(cls.engine)
 
-    def wait_for_service(self, url, max_attempts=10):
-        # Ожидаем доступности сервиса по указанному URL-адресу
-        attempts = 0
-        while attempts < max_attempts:
-            try:
-                requests.get(url)
-                break
-            except requests.ConnectionError:
-                time.sleep(1)
-                attempts += 1
+    def test_create_food_delivery_and_record(self):
+        # Тест создания доставки еды и записи в базу данных
+        order_id = 12345
+        result = create_food_delivery_and_record(self.session, order_id)
 
-    def setUp(self):
-        # Ожидаем доступности сервиса перед каждым тестом
-        self.wait_for_service('http://localhost:8000')
+        # Проверяем, что доставка создана и записана в базу данных
+        self.assertIn("food_delivery_id", result)
+        self.assertIn("payment_status", result)
+        self.assertIn("food_status", result)
 
-    def test_create_food_delivery(self):
-        # Тест создания доставки еды и проверки HTTP-статуса
-        res = requests.post('http://localhost:8000/food-delivery/2')
-        self.assertEqual(res.status_code, 200)
+        # Проверяем, что доставка присутствует в базе данных
+        db_food_delivery = self.session.query(FoodDelivery).filter(FoodDelivery.order_id)
 
-    def test_get_data_of_food_delivery(self):
-        # Тест получения данных о доставке еды и проверки содержимого
-        res = requests.get('http://localhost:8000/food-delivery/2').json()
-        self.assertIn("order_id", res)
-        self.assertIn("status", res)
-        self.assertEqual(res["order_id"], 2)
+    def test_create_food_delivery_duplicate(self):
+        # Создаем доставку с определенным order_id
+        order_id = 12345
+        create_food_delivery_and_record(self.session, order_id)
 
-    def test_fetch_food_delivery(self):
-        # Тест на запрос данных о несуществующей доставке еды и проверку HTTP-статуса
-        res = requests.get('http://localhost:8000/food-delivery/100')
-        self.assertEqual(res.status_code, 404)
+        # Пытаемся создать доставку с тем же order_id еще раз
+        # Ожидаем IntegrityError, так как заказ уже существует
+        try:
+            create_food_delivery_and_record(self.session, order_id)
+        except IntegrityError as e:
+            print("IntegrityError occurred:", e)
+            raise
 
-    def test_cancel_food_delivery(self):
-        # Тест на отмену доставки еды и проверку HTTP-статуса
-        res = requests.delete('http://localhost:8000/food-delivery/cancel/1')
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue('message' in res.json())
 
 if __name__ == '__main__':
     unittest.main()
