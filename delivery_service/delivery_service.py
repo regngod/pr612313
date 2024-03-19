31d441cb5b33b1e51e3a8fdc3b2f437e3d1cba8c
alexandrkorol <oaangrycreeper10@gmail.com>
2024-03-19 14:33:08 +0300

    туц

--- delivery_service/delivery_service.py
+++ delivery_service/delivery_service.py
@@ -1,17 +1,64 @@
 import sqlalchemy
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, status, Form
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import uvicorn
+import os
+from keycloak.keycloak_openid import KeycloakOpenID
 
 app = FastAPI()
 
+KEYCLOAK_URL = "http://0.0.0.0:8180/"
+KEYCLOAK_CLIENT_ID = "mandzhiev"
+KEYCLOAK_REALM = "prc9"
+KEYCLOAK_CLIENT_SECRET = "xYR5e9THASbiSfW9XdlsKaLxFWGbwlK1"
+#KEYCLOAK_CLIENT_SECRET2 = "AliwqwCWP6RvWgENTnWaFvopI3G5s7lm"
+
+keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
+                                  client_id=KEYCLOAK_CLIENT_ID,
+                                  realm_name=KEYCLOAK_REALM,
+                                  client_secret_key=KEYCLOAK_CLIENT_SECRET)
+
+user_token = ""
+
+
+@app.post("/get-token")
+async def get_token(username: str = Form(...), password: str = Form(...)):
+    try:
+        # Получение токена
+        token = keycloak_openid.token(grant_type=["password"],
+                                      username=username,
+                                      password=password)
+        global user_token
+        user_token = token
+        return token
+    except Exception as e:
+        print(e)  # Логирование для диагностики
+        raise HTTPException(status_code=400, detail="Не удалось получить токен")
+
+
+def check_user_roles():
+    global user_token
+    token = user_token
+    try:
+        # userinfo = keycloak_openid.userinfo(token["access_token"])
+        token = keycloak_openid.token("regngod2", "12345")
+        token_info = keycloak_openid.introspect(token["access_token"])
+        if "admin1" not in token_info["realm_access"]["roles"]:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return token_info
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
+
 # Подключение к базе данных
-DATABASE_URL = "sqlite:///./food_delivery.db"  # Изменил имя базы данных
+current_directory = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(current_directory, "food_delivery.db")
+DATABASE_URL = f"sqlite:///{db_path}"
 engine = create_engine(DATABASE_URL)
 
 # Создание базового класса для моделей SQLAlchemy
-Base = sqlalchemy.orm.declarative_base()
+Base = declarative_base()
 
 # Определение модели FoodDelivery
 class FoodDelivery(Base):
@@ -52,24 +99,34 @@ def create_food_delivery_and_record(db, order_id: int):
 # POST-запрос для создания доставки еды
 @app.post("/food-delivery/{order_id}")
 def create_food_delivery(order_id: int):
-    db = SessionLocal()
-    result = create_food_delivery_and_record(db, order_id)
-    db.close()
-    return result
+    if (check_user_roles()):
+        db = SessionLocal()
+        result = create_food_delivery_and_record(db, order_id)
+        db.close()
+        return result
+    else:
+        return "Wrong JWT Token"
 
 # GET-запрос для чтения данных о доставке еды из БД
 @app.get("/food-delivery/{order_id}")
 def read_food_delivery(order_id: int):
-    db = SessionLocal()
-    food_delivery = db.query(FoodDelivery).filter(FoodDelivery.order_id == order_id).first()
-    db.close()
+    if (check_user_roles()):
+        db = SessionLocal()
+        food_delivery = db.query(FoodDelivery).filter(FoodDelivery.order_id == order_id).first()
+        db.close()
 
-    if not food_delivery:
-        raise HTTPException(status_code=404, detail="Food delivery not found")
+        if not food_delivery:
+            raise HTTPException(status_code=404, detail="Food delivery not found")
 
-    return {
-        "order_id": food_delivery.order_id,
-        "status": food_delivery.status,
-        "payment_status": "paid" if food_delivery.status == "delivered" else "pending",
-        "food_status": "prepared",
-    }
+        return {
+            "order_id": food_delivery.order_id,
+            "status": food_delivery.status,
+            "payment_status": "paid" if food_delivery.status == "delivered" else "pending",
+            "food_status": "prepared",
+        }
+    else:
+        return "Wrong JWT Token"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
--- payment_service/payment_service.py
+++ payment_service/payment_service.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, HTTPException
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import uvicorn
+import os
+
 
 app = FastAPI()
 
@@ -54,3 +57,5 @@ def read_payment(order_id: int):
         raise HTTPException(status_code=404, detail="Payment not found")
 
     return {"order_id": payment.order_id, "status": payment.status}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
