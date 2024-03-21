8297306059d7fa7987a37edbebfc9711a0b25369
alexandrkorol <oaangrycreeper10@gmail.com>
2024-03-21 11:22:09 +0300

    туц

--- tests/component.py
+++ tests/component.py
@@ -7,7 +7,7 @@ class TestFoodDelivery(unittest.TestCase):
         # Отправляем POST-запрос на /get-token с неправильными учетными данными
         username = "aaaaa"
         password = "123"
-        response = requests.post("http://localhost:8080/get-token", data={"username": username, "password": password})
+        response = requests.post("http://localhost:8000/get-token", data={"username": username, "password": password})
 
         # Проверяем, что сервер возвращает код статуса
         self.assertEqual(response.status_code, 400)
@@ -15,7 +15,7 @@ class TestFoodDelivery(unittest.TestCase):
     def test_read_food_delivery(self):
         # Отправляем GET-запрос на /food-delivery/{order_id}
         order_id = 123 
-        response = requests.get(f"http://localhost:8080/food-delivery/{order_id}")
+        response = requests.get(f"http://localhost:8000/food-delivery/{order_id}")
 
         # Проверяем, что сервер возвращает код статуса
         self.assertEqual(response.status_code, 401)
